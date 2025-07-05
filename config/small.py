4f18685005ed1ef9e5eff79488a143c031292374
daviddcho <amazingdcho@gmail.com>
2025-07-05 18:26:20 -0400

    increase

--- config/small.py
+++ config/small.py
@@ -1,10 +1,11 @@
 from types import SimpleNamespace
 
 cfg = SimpleNamespace(
+  N_EPOCHS = 10000,
   D_MODEL = 768,
   N_HEADS = 12,
   N_LAYERS = 10,
   D_FF = 2048,
-  MAX_SEQ_LEN = 512,
+  MAX_SEQ_LEN = 1024,
   BS = 8,
 )
